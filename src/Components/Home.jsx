f92e5c053f69a7d49b0a68e472c116614ef3c4f0
pratik <pbsawant2000@gmail.com>
2022-08-01 23:34:19 +0530

    initial commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="h-full flex flex-col justify-center items-center text-white">
+            <p>Hello,</p>
+            <p>I am Pratik</p>
+        </div>
+    )
+}
+
+export default Home
