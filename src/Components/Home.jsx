57faa2689e959a1f27026e05e6350b12b0bd3aae
pratik <pbsawant2000@gmail.com>
2022-08-12 00:16:24 +0530

    home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
     return (
         <div className="h-full flex flex-col justify-center items-center text-white">
-            <p>Hello,</p>
-            <p>I am Pratik</p>
+            <p className="text-9xl mb-6 name-text">Pratik Sawant</p>
+            <p className="text-2xl ">Full Stack Web Developer</p>
         </div>
     )
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa';
 function Sidebar() {
     return (
         <div className="sidebar-container h-full shadow-md flex flex-col justify-around">
-            <h1 className="logo flex justify-center text-white overflow-hidden">PS</h1>
+            <h1 className="logo flex justify-center text-white overflow-hidden"><span className="logo-text">PS</span> </h1>
             <ul className="link-container flex flex-col justify-center">
                 <li className="sidebar-links">
                     <a className="flex justify-center py-4 h-12 overflow-hidden text-gray-400 text-ellipsis whitespace-nowrap rounded hover:text-white transition duration-300 ease-in-out" href="#!">Home</a>
@@ -33,8 +33,8 @@ function Sidebar() {
                 </li>
             </ul>
             <div className="other-links flex justify-evenly my-10">
-                <FaLinkedin className="text-white text-xl" />
-                <FaGithub className="text-white text-xl" />
+                <FaLinkedin className="text-white text-xl links" />
+                <FaGithub className="text-white text-xl links" />
                 {/* <FaLinkedin className="text-white text-xl" /> */}
             </div>
         </div>
